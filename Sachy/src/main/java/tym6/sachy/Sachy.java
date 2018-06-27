46b41030bccc61c6ab979bae267e94aa018b6374
valecd <domivalec@gmail.com>
2018-06-27 14:01:17 +0200

    Gui

---
+++ Sachy/src/main/java/tym6/sachy/Sachy.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tym6.sachy;
+
+import tym6.sachy.Okno;
+
+/**
+ *
+ * @author NIKVAL
+ */
+public class Sachy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Okno okno = new Okno();
+        okno.setVisible(true);
+    }
+    
+}
