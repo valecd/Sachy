2dbe1c0360981c8bda130ebb0a387acc25ff316a
macalibor <40519143+macalibor@users.noreply.github.com>
2018-06-28 23:11:28 +0200

    Add files via upload

---
+++ Sachy/src/test/java/tym6/sachy/HraTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tym6.sachy;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Libor
+ */
+public class HraTest {
+
+    @Test
+    public void testRozestaveniDamaBila() {
+
+        Hra hra = new Hra();
+        List<Figurka> figurky = new ArrayList<>();
+        figurky.add(new Dama(Barva.BILA));
+
+        Dama instance = (Dama) figurky.get(0);
+        int expectedX = 3;
+        int resultX = instance.getSouradnice().getX();
+        int expectedY = 7;
+        int resultY = instance.getSouradnice().getY();
+        
+        assertEquals(expectedX, resultX);
+        assertEquals(expectedY, resultY);
+    }
+}
