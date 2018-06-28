f21d6dd5a4d9172f930d69bef9cc87e800286300
BeranPavel <40557644+BeranPavel@users.noreply.github.com>
2018-06-28 23:33:53 +0200

    Přidání pěšce

--- Sachy/src/main/java/tym6/sachy/Pesec.java
+++ Sachy/src/main/java/tym6/sachy/Pesec.java
@@ -12,9 +12,44 @@ public class Pesec extends Figurka {
      *
      * @param barva
      */
-    public Pesec(Barva barva) {
-       // super(barva);
+    private int[][] polePohybu = new int[8][8];
+    
+    Pesec(Barva barva) {
+        super.hra = hra;
+        super.barva = barva;
+        setJmeno("Pesec");
         if (barva.equals(Barva.BILA)) {
+            setSouradnice(2, 7);
+
+        } else {
+            setSouradnice(2, 1);
         }
+    }
+    
+    public int[][] mozneTahy() {
+        polePohybu = new int[8][8];
+        Souradnice xy = new Souradnice(this.getSouradnice().getX(), this.getSouradnice().getY());
+        if (true) {
+            if (barva == Barva.BILA) {
+                
+            xy = Pohyb.PosunUp(xy.getX(), xy.getY());
+            if (xy.getX() == -1 && xy.getY() == -1) {
+            } else {
+                polePohybu[xy.getX()][xy.getY()] = 1;
+            }
+        }
+            else if (barva == Barva.CERNA){
+                xy = Pohyb.PosunDown(xy.getX(), xy.getY());
+            if (xy.getX() == -1 && xy.getY() == -1) {
+            } else {
+                polePohybu[xy.getX()][xy.getY()] = 1;
+            }
+            }
+        }
+        xy.setX(this.getSouradnice().getX());
+        xy.setY(this.getSouradnice().getY());
+        
+        return polePohybu;
+        
     }
 }
