a27fd2ccd37ae68a67f0f0ba1acea2bce170804c
macalibor <l.m.ml@seznam.cz>
2018-06-28 22:51:03 +0200

    Testy.

---
+++ Sachy/src/test/java/tym6/sachy/DamaTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tym6.sachy;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Libor
+ */
+public class DamaTest {
+
+    @Test
+    public void testMozneTahy() {
+
+        Dama instance = new Dama(Barva.BILA);
+        instance.setSouradnice(5, 5);
+        int[][] expResult = {
+            {1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 0, 0, 1}};
+        int[][] result = instance.mozneTahy();
+        assertArrayEquals(expResult, result);
+    }
+
+}
