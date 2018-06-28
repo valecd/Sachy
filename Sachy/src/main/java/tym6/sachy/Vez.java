95002b10a72377d3d6abffab325e6cd9d5e9b868
macalibor <l.m.ml@seznam.cz>
2018-06-28 08:49:23 +0200

    Test Commit

--- Sachy/src/main/java/tym6/sachy/Vez.java
+++ Sachy/src/main/java/tym6/sachy/Vez.java
@@ -22,22 +22,7 @@ public class Vez implements Figurka {
 
     @Override
     public void mozneTahy() {
-        int xy[] = {souradnice[0], souradnice[1]};
-        while(true){
-            xy = Pohyb.PosunUpLeft(xy[0], xy[1]);
-            if(xy[0]==-1&&xy[1]==-1){
-                break;
-            }else{
-                polePohybu[xy[0]][xy[1]] = 1;
-            }
-        }
-        
-        for(int i=0; i<8; i++){
-            for(int j=0; j<8; j++){
-                System.out.print(polePohybu[j][i]);
-            }
-            System.out.println("");
-        }
+
         
     }
 
