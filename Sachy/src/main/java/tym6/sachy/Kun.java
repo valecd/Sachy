71c6a6f52950515ee29c4984eee9cec173a54ef3
BeranPavel <40557644+BeranPavel@users.noreply.github.com>
2018-06-29 08:57:17 +0200

    Přidání koně

--- Sachy/src/main/java/tym6/sachy/Kun.java
+++ Sachy/src/main/java/tym6/sachy/Kun.java
@@ -7,14 +7,62 @@ package tym6.sachy;
  * @author Valeczek, Maca, Beran, Pavlik
  */
 public class Kun extends Figurka {
+    
+    private int[][] polePohybu = new int[8][8];
+    private char barva;
 
     /**
      *
      * @param barva
      */
-    public Kun(Barva barva) {
-       // super(barva);
-        if (barva.equals(Barva.BILA)) {
+    
+    public int[][] mozneTahy() {
+        polePohybu = new int[8][8];
+        Souradnice xy = new Souradnice(this.getSouradnice().getX(), this.getSouradnice().getY());
+        if (true) {
+            //System.out.println(xy.getX()+" "+xy.getY());
+
+            xy = Pohyb.KunPosunUpRight(xy.getX(), xy.getY());
+            if (xy.getX() == -1 && xy.getY() == -1) {
+//                break;
+            } else {
+                polePohybu[xy.getX()][xy.getY()] = 1;
+            }
         }
+        xy.setX(this.getSouradnice().getX());
+        xy.setY(this.getSouradnice().getY());
+        if (true) {
+            xy = Pohyb.KunPosunDownRight(xy.getX(), xy.getY());
+            if (xy.getX() == -1 && xy.getY() == -1) {
+//                break;
+            } else {
+                polePohybu[xy.getX()][xy.getY()] = 1;
+            }
+        }
+        xy.setX(this.getSouradnice().getX());
+        xy.setY(this.getSouradnice().getY());
+        if (true) {
+            xy = Pohyb.KunPosunUpLeft(xy.getX(), xy.getY());
+            if (xy.getX() == -1 && xy.getY() == -1) {
+//                break;
+            } else {
+                polePohybu[xy.getX()][xy.getY()] = 1;
+            }
+        }
+        xy.setX(this.getSouradnice().getX());
+        xy.setY(this.getSouradnice().getY());
+        if (true) {
+            xy = Pohyb.KunPosunDownLeft(xy.getX(), xy.getY());
+            if (xy.getX() == -1 && xy.getY() == -1) {
+//                break;
+            } else {
+                polePohybu[xy.getX()][xy.getY()] = 1;
+            }
+        }
+    
+
+        
+        return polePohybu;
     }
+    
 }
