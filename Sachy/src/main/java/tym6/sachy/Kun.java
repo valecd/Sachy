7d1ea5cdd546644c771eef01b6fc269efdcbec4d
BeranPavel <40557644+BeranPavel@users.noreply.github.com>
2018-06-29 09:17:22 +0200

    Přidání pohybu koně

--- Sachy/src/main/java/tym6/sachy/Kun.java
+++ Sachy/src/main/java/tym6/sachy/Kun.java
@@ -27,11 +27,11 @@ public class Kun extends Figurka {
         polePohybu = new int[8][8];
         Souradnice xy = new Souradnice(this.getSouradnice().getX(), this.getSouradnice().getY());
         if (true) {
-            //System.out.println(xy.getX()+" "+xy.getY());
+           
 
             xy = Pohyb.KunPosunUpRight(xy.getX(), xy.getY());
             if (xy.getX() == -1 && xy.getY() == -1) {
-//                break;
+
             } else {
                 polePohybu[xy.getX()][xy.getY()] = 1;
             }
@@ -41,7 +41,6 @@ public class Kun extends Figurka {
         if (true) {
             xy = Pohyb.KunPosunDownRight(xy.getX(), xy.getY());
             if (xy.getX() == -1 && xy.getY() == -1) {
-//                break;
             } else {
                 polePohybu[xy.getX()][xy.getY()] = 1;
             }
@@ -51,7 +50,6 @@ public class Kun extends Figurka {
         if (true) {
             xy = Pohyb.KunPosunUpLeft(xy.getX(), xy.getY());
             if (xy.getX() == -1 && xy.getY() == -1) {
-//                break;
             } else {
                 polePohybu[xy.getX()][xy.getY()] = 1;
             }
@@ -61,7 +59,44 @@ public class Kun extends Figurka {
         if (true) {
             xy = Pohyb.KunPosunDownLeft(xy.getX(), xy.getY());
             if (xy.getX() == -1 && xy.getY() == -1) {
+            } else {
+                polePohybu[xy.getX()][xy.getY()] = 1;
+            }
+        }
+        
+        xy.setX(this.getSouradnice().getX());
+        xy.setY(this.getSouradnice().getY());
+        if (true) {
+            xy = Pohyb.KunPosunUpRightRight(xy.getX(), xy.getY());
+            if (xy.getX() == -1 && xy.getY() == -1) {
+            } else {
+                polePohybu[xy.getX()][xy.getY()] = 1;
+            }
+        }
+        xy.setX(this.getSouradnice().getX());
+        xy.setY(this.getSouradnice().getY());
+        if (true) {
+            xy = Pohyb.KunPosunDownRightRight(xy.getX(), xy.getY());
+            if (xy.getX() == -1 && xy.getY() == -1) {
 //                break;
+            } else {
+                polePohybu[xy.getX()][xy.getY()] = 1;
+            }
+        }
+        xy.setX(this.getSouradnice().getX());
+        xy.setY(this.getSouradnice().getY());
+        if (true) {
+            xy = Pohyb.KunPosunDownLeftLeft(xy.getX(), xy.getY());
+            if (xy.getX() == -1 && xy.getY() == -1) {
+            } else {
+                polePohybu[xy.getX()][xy.getY()] = 1;
+            }
+        }
+        xy.setX(this.getSouradnice().getX());
+        xy.setY(this.getSouradnice().getY());
+        if (true) {
+            xy = Pohyb.KunPosunUpLeftLeft(xy.getX(), xy.getY());
+            if (xy.getX() == -1 && xy.getY() == -1) {
             } else {
                 polePohybu[xy.getX()][xy.getY()] = 1;
             }
